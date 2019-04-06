94fe5a665ebe178b2c21c51ced98c076ddd3d159
Kevin Li <kevinli020508@gmail.com>
2019-04-06 13:44:02 -0400

    Move _detectMouseMove to constructor and hide hover box outside container

--- script.js
+++ script.js
@@ -17,63 +17,57 @@ class ElementPicker {
             ...options
         };
         Object.keys(mergedOptions).forEach((key) => {
-            if (key !== "container"){ // initialize container last
-                this[key] = mergedOptions[key];
-            }
+            this[key] = mergedOptions[key];
         });
-        
-        this.container = mergedOptions.container;
+
+        this._detectMouseMove = (e) => {
+            this._previousEvent = e;
+            let target = e.target;
+            // console.log("TCL: ElementPicker -> this._moveHoverBox -> target", target)
+            if (this.ignoreElements.indexOf(target) === -1 && target.matches(this.selectors) &&
+                this.container.contains(target) ||
+                target === this.hoverBox) { // is NOT ignored elements
+                // console.log("TCL: target", target);
+                if (target === this.hoverBox) {
+                    // the truely hovered element behind the added hover box
+                    const hoveredElement = document.elementsFromPoint(e.clientX, e.clientY)[1];
+                    // console.log("screenX: " + e.screenX);
+                    // console.log("screenY: " + e.screenY);
+                    // console.log("TCL: hoveredElement", hoveredElement);
+                    if (this._previousTarget === hoveredElement) {
+                        // avoid repeated calculation and rendering
+                        return;
+                    } else {
+                        target = hoveredElement;
+                    }
+                } else {
+                    this._previousTarget = target;
+                }
+                const targetOffset = target.getBoundingClientRect();
+                const targetHeight = targetOffset.height;
+                const targetWidth = targetOffset.width;
+
+                this.hoverBox.style.width = targetWidth + this.borderWidth * 2 + "px";
+                this.hoverBox.style.height = targetHeight + this.borderWidth * 2 + "px";
+                // need scrollX and scrollY to account for scrolling
+                this.hoverBox.style.top = targetOffset.top + window.scrollY - this.borderWidth + "px";
+                this.hoverBox.style.left = targetOffset.left + window.scrollX - this.borderWidth + "px";
+            } else {
+                // console.log("hiding hover box...");
+                this.hoverBox.style.width = 0;
+            }
+        };
+        document.addEventListener("mousemove", this._detectMouseMove);
     }
     get container() {
         return this._container;
     }
     set container(value) {
         if (value instanceof HTMLElement) {
-            if (this.container){
-                this.container.removeEventListener("mousemove", this._moveHoverBox);
-            }
-
             this._container = value;
-            this._moveHoverBox = (e) => {
-                this._previousEvent = e;
-                let target = e.target;
-				// console.log("TCL: ElementPicker -> this._moveHoverBox -> target", target)
-                if (this.ignoreElements.indexOf(target) === -1 && target.matches(this.selectors)
-                    || target === this.hoverBox) { // is NOT ignored elements
-                    // console.log("TCL: target", target);
-                    if (target === this.hoverBox) {
-                        // the truely hovered element behind the added hover box
-                        const hoveredElement = document.elementsFromPoint(e.clientX, e.clientY)[1];
-                        // console.log("screenX: " + e.screenX);
-                        // console.log("screenY: " + e.screenY);
-                        // console.log("TCL: hoveredElement", hoveredElement);
-                        if (this._previousTarget === hoveredElement) {
-                            // avoid repeated calculation and rendering
-                            return;
-                        } else {
-                            target = hoveredElement;
-                        }
-                    } else {
-                        this._previousTarget = target;
-                    }
-                    const targetOffset = target.getBoundingClientRect();
-                    const targetHeight = targetOffset.height;
-                    const targetWidth = targetOffset.width;
-
-                    this.hoverBox.style.width = targetWidth + this.borderWidth * 2 + "px";
-                    this.hoverBox.style.height = targetHeight + this.borderWidth * 2 + "px";
-                    // need scrollX and scrollY to account for scrolling
-                    this.hoverBox.style.top = targetOffset.top + window.scrollY - this.borderWidth + "px";
-                    this.hoverBox.style.left = targetOffset.left + window.scrollX - this.borderWidth + "px";
-                } else{
-                    // console.log("hiding hover box...");
-                    this.hoverBox.style.width = 0;
-                }
-            };
-            this.container.addEventListener("mousemove", this._moveHoverBox);
             this.container.appendChild(this.hoverBox);
-        } else{
-            throw new Error("Please specify an HTML element as container!");
+        } else {
+            throw new Error("Please specify an HTMLElement as container!");
         }
     }
     get background() {
@@ -92,34 +86,33 @@ class ElementPicker {
 
         this.hoverBox.style.transition = this.transition;
     }
-    get borderWidth(){
+    get borderWidth() {
         return this._borderWidth;
     }
-    set borderWidth(value){
+    set borderWidth(value) {
         this._borderWidth = value;
 
-        if (this._moveHoverBox){
-            this._moveHoverBox(this._previousEvent);
-        }
+        this._redetectMouseMove();
     }
-    get selectors(){
+    get selectors() {
         return this._selectors;
     }
-    set selectors(value){
+    set selectors(value) {
         this._selectors = value;
 
-        if (this._moveHoverBox){
-            this._moveHoverBox(this._previousEvent);
-        }
+        this._redetectMouseMove();
     }
-    get ignoreElements(){
+    get ignoreElements() {
         return this._ignoreElements;
     }
-    set ignoreElements(value){
+    set ignoreElements(value) {
         this._ignoreElements = value;
 
-        if (this._moveHoverBox){
-            this._moveHoverBox(this._previousEvent);
+        this._redetectMouseMove();
+    }
+    _redetectMouseMove() {
+        if (this._detectMouseMove && this._previousEvent) {
+            this._detectMouseMove(this._previousEvent);
         }
     }
 }
