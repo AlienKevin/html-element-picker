9740dec64b2539dd4e4f7caf1ca14774bd531778
Kevin Li <kevinli020508@gmail.com>
2019-04-06 10:23:06 -0400

    Extract to ElementPicker class with many options

--- script.js
+++ script.js
@@ -1,42 +1,67 @@
-const hoverBox = document.createElement("div");
-console.log("hoverBox: ", hoverBox);
-hoverBox.style.position = "absolute";
-// change to whatever highlight color you want
-hoverBox.style.background = "rgba(153, 235, 255, 0.5)";
-// avoid blocking the hovered element and its surroundings
-hoverBox.style.zIndex = "0";
-document.body.appendChild(hoverBox);
-let previousTarget;
-document.addEventListener("mousemove", (e) => {
-    let target = e.target;
-    console.log("TCL: target", target);
-    if (target === hoverBox) {
-        // the truely hovered element behind the added hover box
-        const hoveredElement = document.elementsFromPoint(e.clientX, e.clientY)[1];
-        console.log("screenX: " + e.screenX);
-        console.log("screenY: " + e.screenY);
-		console.log("TCL: hoveredElement", hoveredElement);
-        if (previousTarget === hoveredElement){
-            // avoid repeated calculation and rendering
-            return;
-        } else{
-            target = hoveredElement;
+class ElementPicker {
+    constructor(options) {
+        const defaultOptions = {
+            container: document.body,
+            selectors: "*", // default to pick all elements
+            background: "rgba(153, 235, 255, 0.5)", // transparent light blue
+            borderWidth: 5,
+            transition: "all 150ms ease", // set to "" (empty string) to disable
+            ignoreElements: [document.body],
         }
-    } else{
-        previousTarget = target;
+        const mergedOptions = {
+            ...defaultOptions,
+            ...options
+        };
+        Object.keys(mergedOptions).forEach((key) => {
+            this[key] = mergedOptions[key];
+        });
+
+        const hoverBox = document.createElement("div");
+        // console.log("hoverBox: ", hoverBox);
+        hoverBox.style.position = "absolute";
+        // change to whatever highlight color you want
+        hoverBox.style.background = this.background;
+        // avoid blocking the hovered element and its surroundings
+        hoverBox.style.zIndex = "0";
+        hoverBox.style.transition = this.transition;
+        this.container.appendChild(hoverBox);
+        let previousTarget;
+        this.container.addEventListener("mousemove", (e) => {
+            let target = e.target;
+            if (this.ignoreElements.indexOf(target) === -1 && target.matches(this.selectors)) { // is NOT ignored elements
+                // console.log("TCL: target", target);
+                if (target === hoverBox) {
+                    // the truely hovered element behind the added hover box
+                    const hoveredElement = document.elementsFromPoint(e.clientX, e.clientY)[1];
+                    // console.log("screenX: " + e.screenX);
+                    // console.log("screenY: " + e.screenY);
+                    // console.log("TCL: hoveredElement", hoveredElement);
+                    if (previousTarget === hoveredElement) {
+                        // avoid repeated calculation and rendering
+                        return;
+                    } else {
+                        target = hoveredElement;
+                    }
+                } else {
+                    previousTarget = target;
+                }
+                const targetOffset = target.getBoundingClientRect();
+                const targetHeight = targetOffset.height;
+                const targetWidth = targetOffset.width;
+
+                hoverBox.style.width = targetWidth + this.borderWidth * 2 + "px";
+                hoverBox.style.height = targetHeight + this.borderWidth * 2 + "px";
+                // need scrollX and scrollY to account for scrolling
+                hoverBox.style.top = targetOffset.top + window.scrollY - this.borderWidth + "px";
+                hoverBox.style.left = targetOffset.left + window.scrollX - this.borderWidth + "px";
+            }
+        });
     }
-    const targetOffset = target.getBoundingClientRect();
-    const targetHeight = targetOffset.height;
-    const targetWidth = targetOffset.width;
-    // add a border around hover box
-    const boxBorder = 5;
-    hoverBox.style.width = targetWidth + boxBorder * 2 + "px";
-    hoverBox.style.height = targetHeight + boxBorder * 2 + "px";
-    // need scrollX and scrollY to account for scrolling
-    hoverBox.style.top = targetOffset.top + window.scrollY - boxBorder + "px";
-    hoverBox.style.left = targetOffset.left + window.scrollX - boxBorder + "px";
+}
+new ElementPicker({
+    background: "rgba(20,20,20,0.5)",
+    transition: "",
+    // container: document.querySelector("main"),
+    // borderWidth: -5,
+    // selectors: "button",
 });
-// document.addEventListener("click", (e) => {
-//     const target = e.target;
-//     target.remove();
-// });
